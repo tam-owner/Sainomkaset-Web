c34513d5ad6b1bb137432b6f5bab7db8070d5e75
Tam <2owner.sainom@gmail.com>
2026-06-15 19:45:59 +0700

    Change net pay box to light green

--- js/app.js
+++ js/app.js
@@ -625,7 +625,7 @@ function renderEmployeeDashboard() {
             ` : ''}
 
             <!-- Net Pay (Grand Total) -->
-            <div class="bg-[#022c22] rounded-[20px] p-5 flex justify-between items-center shadow-lg shadow-[#022c22]/40 mt-4 relative overflow-hidden">
+            <div class="bg-[#0fa981] rounded-[20px] p-5 flex justify-between items-center shadow-lg shadow-[#0fa981]/40 mt-4 relative overflow-hidden">
                 <div class="relative z-10">
                     <p class="text-[11px] font-black text-emerald-50 uppercase tracking-widest">รวมรายได้สุทธิ</p>
                     <p class="text-[10px] text-emerald-100 mt-1 font-medium">หลังหักทั้งหมด (Net Pay)</p>
